38af146d5f10caa36e9c5e6bde8784ffdceda85b
Moisés Tedeschi <moisestedeschi@gmail.com>
2020-05-17 00:30:22 -0300

    Implementando testes de colisão e calculos de posição vertical.

--- atores.py
+++ atores.py
@@ -67,6 +67,7 @@ class Obstaculo(Ator):
 
 class Porco(Ator):
     _caracter_ativo = '@'
+    _caracter_destruido = '+'
 
 class DuploLancamentoExcecao(Exception):
     pass
@@ -97,7 +98,7 @@ class Passaro(Ator):
 
         :return: booleano
         """
-        return True
+        return not self._tempo_de_lancamento is None
 
     def colidir_com_chao(self):
         """
@@ -121,7 +122,11 @@ class Passaro(Ator):
         :param tempo: tempo de jogo a ser calculada a posição
         :return: posição x, y
         """
-        return 1, 1
+        if self.foi_lancado():
+            delta_t = tempo - self._tempo_de_lancamento
+            self._calcular_posicao_vertical(delta_t)
+            self._calcular_posicao_horizontal(delta_t)
+        return super().calcular_posicao(tempo)
 
 
     def lancar(self, angulo, tempo_de_lancamento):
@@ -133,12 +138,30 @@ class Passaro(Ator):
         :param tempo_de_lancamento:
         :return:
         """
+        self._angulo_de_lancamento = angulo
+        self._tempo_de_lancamento = tempo_de_lancamento
+
+    def _calcular_posicao_vertical(self, delta_t):
+        y_atual = self._y_inicial
+        # Transformando o ângulo - O ângulo é passado em graus e deve ser transformado em radianos.
+        angulo_radianos = math.radians(self._angulo_de_lancamento)
+        y_atual += self.velocidade_escalar * delta_t * math.sin(angulo_radianos)
+        # Calculando a gravidade
+        y_atual -= (GRAVIDADE * (delta_t ** 2)) / 2
+
+        self.y = y_atual
+
+    def _calcular_posicao_horizontal(self, delta_t):
         pass
 
 
 class PassaroAmarelo(Passaro):
-    pass
+    _caracter_ativo = 'A'
+    _caracter_destruido = 'a'
+    velocidade_escalar = 30
 
 
 class PassaroVermelho(Passaro):
     _caracter_ativo = 'V'
+    _caracter_destruido = 'v'
+    velocidade_escalar = 20
